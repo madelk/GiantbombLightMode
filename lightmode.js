3adddd45a0448cfd5b7dcdaddc5d52307c752253
Mark Dell <madelk@gmail.com>
2018-11-29 20:10:35 -0600

    Circles and insensitivity

--- lightmode.js
+++ lightmode.js
@@ -18,19 +18,22 @@ var replaceValues = {
     "#242628": "#DBD9D7",
     "#000": "#ffe",
     "#fff": "#001",
-    "#f7f7f8": "#080808",
-    "#999": "#666",
-    "#191919": "#E6E6E6",
+    "#f7f7f8": "#080809",
+    "#999": "#666667",
+    "#191919": "#E6E6E7",
     "#00ffd4": "red",
-    "#404040": "#BFBFBF",
-    "#262626": "#D9D9D9"
+    "#404040": "#BFBFC1",
+    "#262626": "#D9D9E0",
+    "#BFBFBF" : "404041",
+    "#f2f2f2": "#0D0D0E",
+    "255,255,255,0.25": "0,0,0,1.25"
 }
 function hackit(css) {
+    // This kind of sucks. It'd be neat if it found all the places to change and then did them all at once rather than editing the same string over and over
     Object.keys(replaceValues).forEach(valueToReplace => {
-        var re = new RegExp(valueToReplace,"g");
+        var re = new RegExp(valueToReplace,"gi");
         css = css.replace(re, replaceValues[valueToReplace]);
     });
-    // return css.replace(/#242628/g, "#DBD9D7").replace(/#000/g, "#tempbutts").replace(/#fff/g, "#000").replace(/#tempbutts/g, "#fff");
     return css;
 }
 doit();
